654d4e9010a93127bb64ab5b59b8c038047854a7
Jordan Knight <jbknight83@gmail.com>
2017-05-23 17:59:05 -0500

    Api keys

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+var googleApi = {
+
+	key : "AIzaSyBUln0pM_BnfEb_h86rcctPuC5hECblXgY",
+	queryUrl : "https://maps.googleapis.com/maps/api/js?key=" + googleApi.key + "&callback=initMap"
+
+};
+
+var firebaseConfig = {
+	apiKey: "AIzaSyBsIeE686XAmx6rsPiE3igmwLIkGm1Iah8",
+    authDomain: "studentmeetup-1495578444077.firebaseapp.com",
+    databaseURL: "https://studentmeetup-1495578444077.firebaseio.com",
+    projectId: "studentmeetup-1495578444077",
+    storageBucket: "studentmeetup-1495578444077.appspot.com",
+    messagingSenderId: "882686649590"
+
+};
+
+firebase.initializeApp(config);
+
+var slackApi = {
+	oauth : "https://slack.com/oauth/authorize",
+	clientId : "159539740338.187078916050",
+	clientSecret : "ebe5869fe6e04851d665a42e8c637c7f"
+
+};
