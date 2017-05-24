d6de9e4587a9e95fb050cf3bc7727b3887b20185
apekearo <apekearo@gmail.com>
2017-05-24 17:11:50 -0500

    Changed the <iframe> to a <div> so that I wouldn't have "Xframe not returning because origin is SAMEORIGIN" issue in the console, also the extra js, make it easier to change its effects,  I ALSO (John) changed, the css iframe to #map  and changed the sizing of the height and width, so it would fit inline,

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,3 +70,54 @@ $.ajax({
             });
 console.log(googlePlaceUrl);
 //*******************************************************************************************************************
+//  Tony GeoLocation Area
+// $.ajax({
+// 			url: "https://www.google.com/maps/embed/v1/geolocate?key=AIzaSyBUln0pM_BnfEb_h86rcctPuC5hECblXgY",
+// 			method: "GET"
+// 		}).done(function(reply){
+// 			var location=reply.data;
+// 			console.log(location);
+// 		});
+
+    
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+   //************************************************************************************
